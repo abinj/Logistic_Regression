7705db54bc0a9c6f351a5b550ce8ea82e4c31d46
abinj <abinmj656@gmail.com>
2020-02-10 22:33:10 -0500

    COnfusion matrix and correlation matrix plotting done

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,10 +1,15 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
 
 # Import the dataset
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+
 
 df = pd.read_csv("voice.csv")
 print(df.head())
@@ -32,3 +37,28 @@ y_pred = logreg.predict(X_test)
 
 print('\nAccuracy score on test data : ', accuracy_score(y_test, y_pred))
 
+
+#Confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+print('Confusion matrix', cm)
+# 0 means Negative and 1 means Positive
+class_names = [0, 1]
+fig, ax = plt.subplots()
+tick_marks = np.arange(len(class_names))
+plt.xticks(tick_marks, class_names)
+plt.yticks(tick_marks, class_names)
+
+sns.heatmap(pd.DataFrame(cm), annot=True, cmap = "YlGnBu", fmt = 'g')
+ax.xaxis.set_label_position("top")
+plt.tight_layout()
+
+plt.title('Confusion matrix', y=1.1)
+plt.ylabel('Actual Label')
+plt.xlabel('Predicted label')
+plt.show()
+
+
+#Correlation matrix
+corrMatrix = df.corr()
+sns.heatmap(corrMatrix, annot=True)
+plt.show()

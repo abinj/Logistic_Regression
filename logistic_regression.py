4c8e1c9b58270ff823bfeda1d9d46bc5e574040c
Kevin Toms <kevinpunnoor@gmail.com>
2020-02-11 16:08:03 -0500

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -62,3 +62,23 @@ plt.show()
 corrMatrix = df.corr()
 sns.heatmap(corrMatrix, annot=True)
 plt.show()
+
+def train_with_the_data(cols, index):
+    X_train = df[cols]
+    Y_train = df['label']
+
+    S_scaler = StandardScaler()
+    X_train = S_scaler.fit_transform(X_train)
+
+    train_X, test_X, train_Y, test_Y = train_test_split(X_train, Y_train, test_size=0.2, random_state=0)
+
+    lr = LogisticRegression()
+    lr.fit(train_X, train_Y)
+
+    y_pred = lr.predict(test_X)
+    print(f'\nAccuracy on {index} cols score on test data : {accuracy_score(test_Y, y_pred)}')
+    
+  
+cols_name = df.columns
+for i in range(1, len(df.columns)):
+    train_with_the_data(cols=cols_name[:i], index=i)

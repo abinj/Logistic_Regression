08174d987f0d13332da5e9b0946255dcb9b29aa1
abinj <abinmj656@gmail.com>
2020-02-10 21:46:52 -0500

    Label encoder part done

--- logistic_regression.py
+++ logistic_regression.py
@@ -1,24 +1,21 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
-
 
 # Import the dataset
-from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from matplotlib.colors import ListedColormap
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 
+df = pd.read_csv("voice.csv")
+print(df.head())
 
-dataset = pd.read_csv("voice.csv")
-X = dataset.iloc[:, [2,3]].values
-Y = dataset.iloc[:, 4].values
+encode = LabelEncoder()
+df.label = encode.fit_transform(df.label)
+
+X = df.iloc[:, df.columns != 'label'].values
+Y = df.iloc[:, 20].values
 
 # Split the dataset into training and test set
-X_train, X_test, y_train, y_test  = train_test_split(X, Y, test_size = 0.20, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, random_state=0)
 
 # Feature Scaling
 sc = StandardScaler()

60dfdb03976c8d0574b4a794f44c5caf2e79c920
abinj <abinmj656@gmail.com>
2020-02-10 22:08:28 -0500

    Y predict result accuracy found

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,17 +2,19 @@ import pandas as pd
 
 # Import the dataset
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 df = pd.read_csv("voice.csv")
 print(df.head())
 
+# Encode Male and Female to 1 and 0
 encode = LabelEncoder()
 df.label = encode.fit_transform(df.label)
 
 X = df.iloc[:, df.columns != 'label'].values
-Y = df.iloc[:, 20].values
+Y = df.iloc[:, -1].values
 
 # Split the dataset into training and test set
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, random_state=0)
@@ -27,3 +29,6 @@ logreg = LogisticRegression(random_state=0)
 logreg.fit(X_train, y_train)
 
 y_pred = logreg.predict(X_test)
+
+print('\nAccuracy score on test data : ', accuracy_score(y_test, y_pred))
+
